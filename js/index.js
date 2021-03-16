69132bb3b4069580265948bcd5e6d2613526de1a
imdeveloperstudent <henao.jhon@correounivalle.edu.co>
2021-03-16 13:42:58 -0500

    js

---
+++ js/index.js
@@ -0,0 +1,28 @@
+ 
+ $(function(){
+    $('#modal-education').modal({
+        backdrop: 'static',
+        focus: true,
+        show: false
+    });
+ })
+
+ $(function(){
+    $('#modal-skills').modal({
+        backdrop: 'static',
+        focus: true,
+        show: false
+    });
+ })
+
+ $(function(){
+    $('#modal-experience').modal({
+        backdrop: 'static',
+        focus: true,
+        show: false
+    });
+ })
+
+
+
+
